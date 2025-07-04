ecfd9f178a17e81dd91fb99c9915980dd6dc62f2
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-07-04 12:55:14 +0800

    compatible for vercel fix

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -36,6 +36,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     console.log('💾 Product saved');
     return res.status(200).json(saveProduct);
   } catch (error: any) {
+    console.log('Error scraping product:', error);
     return res.status(500).json({ error: error.message });
   }
 }
