c77513bca3fb1115cba2c1a2d474f825d8e3a841
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-07-07 09:16:38 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,6 @@ const Home = () => {
                 alt='arrow-right'
                 width={16}
                 height={16}
-                className='w-auto h-auto'
               />
             </p>
 
