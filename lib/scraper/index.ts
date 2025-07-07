f5b7dc6bc1e7d58adb80d31cc0bac9438561ba10
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-07-07 14:55:07 +0800

    redo

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -16,7 +16,7 @@ export async function scrapeProduct(url: string) {
 
     const browser = await puppeteer.launch({
       headless: 'new' as any,
-      executablePath: puppeteer.executablePath(),
+      executablePath: '/root/.cache/puppeteer/chrome/linux-138.0.7204.92/chrome-linux64/chrome',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
