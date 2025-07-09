219d8b4c3ffd10c530b9e34930c62c4a54980ae0
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-07-09 08:32:53 +0800

    enabled

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -21,6 +21,7 @@ export async function scrapeProduct(url: string) {
 
     const browser = await puppeteer.launch({
       headless: 'new' as any,
+      executablePath: puppeteer.executablePath(),
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
